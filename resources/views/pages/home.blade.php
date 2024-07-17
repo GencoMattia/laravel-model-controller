bcd746e028ab106adac45513f94637b40b47598f
GencoMattia <gencomattia@hotmail.it>
2024-07-17 17:02:24 +0200

    successfully linked controller with routes

---
+++ resources/views/pages/home.blade.php
@@ -0,0 +1,11 @@
+@extends("layout.app")
+
+@section("page-title", "Movies Home")
+
+@section("main-content")
+    <main>
+        <h1>
+            SOno un main content
+        </h1>
+    </main>
+@endsection
