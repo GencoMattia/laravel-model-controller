06b907e9e1f505d16f8cfcb119a3b55b30f8eb55
GencoMattia <gencomattia@hotmail.it>
2024-07-17 17:46:36 +0200

    added cards and printed on pages.home

--- resources/views/pages/home.blade.php
+++ resources/views/pages/home.blade.php
@@ -4,8 +4,31 @@
 
 @section("main-content")
     <main>
-        <h1>
-            SOno un main content
-        </h1>
+        <div class="container">
+            <section class="row">
+                @foreach ( $movies as $movie)
+                <div class="card p-0 me-3 mb-4" style="width: 18rem;">
+                    <div class="card-header">
+                        {{ $movie->title }}
+                    </div>
+                    <img src="{{ $movie->cover_image }}" class="card-img-top" alt="{{ $movie->title }} image">
+                    <ul class="list-group list-group-flush">
+                        <li class="list-group-item">
+                            Original Title: {{ $movie->original_title }}
+                        </li>
+                        <li class="list-group-item">
+                            Nationality: {{ $movie->nationality }}
+                        </li>
+                        <li class="list-group-item">
+                            Release: {{ $movie->date }}
+                        </li>
+                        <li class="list-group-item">
+                            Vote: {{ $movie->vote }}
+                        </li>
+                    </ul>
+                </div>
+                @endforeach
+            </section>
+        </div>
     </main>
 @endsection
